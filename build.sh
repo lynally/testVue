4327695b17e2793ddc964e560d1063227f162ec5
liuyn <2212770755@qq.com>
2024-04-29 19:09:14 +0800

    update： 修改构建文件

--- build.sh
+++ build.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
-cd /root
-yarn
-pnpm run build
-
+# 检查 yarn 是否安装
+if ! command -v yarn &> /dev/null
+then
+    echo "Error: Yarn is not installed."
+    exit 1
+fi
+# 安装依赖
+yarn install
+# 构建项目
+yarn build
